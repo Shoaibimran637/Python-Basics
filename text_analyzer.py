70f4a67e9dba52ad1dcc09e89c6fcff108b8b2d7
Shoaibimran637 <151271274+Shoaibimran637@users.noreply.github.com>
2025-05-26 21:32:48 +0500

    Create text_analyzer.py

---
+++ text_analyzer.py
@@ -0,0 +1,6 @@
+# Simple Text Analyzer
+text = input("Enter a text: ")
+print(f"The length of the text is {len(text)}")
+print(f"The number of words in the text is {len(text.split())}")
+print(f"The text in uppercase: {text.upper()}")
+print(f"The text in lowercase: {text.lower()}")
