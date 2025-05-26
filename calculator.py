47f7bc93f060db42b0978864c1e6f456db5a757b
Shoaibimran637 <151271274+Shoaibimran637@users.noreply.github.com>
2025-05-26 21:23:47 +0500

    calculator.py

---
+++ calculator.py
@@ -0,0 +1,14 @@
+# --- Simple Calculator ---
+
+print("This is my first program Calculator")
+
+# Get input from the user (convert to integers)
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Perform basic arithmetic operations and print results
+print(f"""
+      The Addition of {num1} and {num2} is {num1 + num2}
+      The Subtraction of {num1} and {num2} is {num1 - num2}
+      The Multiplication of {num1} and {num2} is {num1 * num2}
+      The Division of {num1} and {num2} is {num1 / num2}""")
